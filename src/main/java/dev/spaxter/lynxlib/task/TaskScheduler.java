785ac7d269876c6e0742647983750e3369c6b290
Kevin Norman <spaxtersurvival@gmail.com>
2024-10-07 04:18:11 +0200

    feat: improve task scheduling logic and allow repeated tasks

--- src/main/java/dev/spaxter/lynxlib/task/TaskScheduler.java
+++ src/main/java/dev/spaxter/lynxlib/task/TaskScheduler.java
@@ -1,7 +1,10 @@
 package dev.spaxter.lynxlib.task;
 
 import dev.spaxter.lynxlib.common.MagicValues;
+import java.util.List;
 import java.util.PriorityQueue;
+import java.util.UUID;
+import java.util.stream.Collectors;
 import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -15,6 +18,10 @@ public class TaskScheduler {
     private static final PriorityQueue<DelayedTask> taskQueue = new PriorityQueue<>();
     private static MinecraftServer server;
 
+    public static void scheduleTask(MinecraftServer server, float delay, Runnable task) {
+        TaskScheduler.scheduleTask(server, delay, task, UUID.randomUUID());
+    }
+
     /**
      * Schedules a task to run after a delay. If {@code delay == 0}, {@code task} will be executed immediately.
      *
@@ -22,16 +29,48 @@ public class TaskScheduler {
      * @param delay  The delay in seconds
      * @param task   The task to run
      */
-    public static void scheduleTask(MinecraftServer server, long delay, Runnable task) {
+    public static void scheduleTask(MinecraftServer server, float delay, Runnable task, UUID taskId) {
         long currentTick = server.getTickCount();
-        long executionTick = currentTick + delay * MagicValues.TICKS_PER_SECOND;
+        long executionTick = Math.round(currentTick + delay * MagicValues.TICKS_PER_SECOND);
 
         if (delay == 0) {
             task.run();
         } else {
-            DelayedTask delayedTask = new DelayedTask(executionTick, task);
+            DelayedTask delayedTask = new DelayedTask(executionTick, task, taskId);
             taskQueue.add(delayedTask);
         }
+    }
+
+    /**
+     * Un-schedule a task.
+     *
+     * @param taskId Task UUID
+     */
+    public static void unScheduleTask(UUID taskId) {
+        List<DelayedTask> task = taskQueue.stream().filter(t -> t.getId().equals(taskId)).collect(Collectors.toList());
+        task.forEach(taskQueue::remove);
+    }
+
+    /**
+     * Schedule a task that repeats every {@code delay} seconds.
+     *
+     * @param server The server to set the task for
+     * @param delay  Delay in seconds
+     * @param task   The task to run
+     * @param taskId UUID of the task
+     */
+    public static void repeatTask(final MinecraftServer server, final float delay, final Runnable task,
+        final UUID taskId) {
+        Runnable repeatedTask = new Runnable() {
+            @Override
+            public void run() {
+                task.run();
+                TaskScheduler.scheduleTask(server, delay, this, taskId);
+            }
+        };
+
+        // Schedule the first instance of the repeated task
+        TaskScheduler.scheduleTask(server, delay, repeatedTask, taskId);
     }
 
     /**
@@ -41,13 +80,15 @@ public class TaskScheduler {
      */
     @SubscribeEvent
     public static void onTick(TickEvent.ServerTickEvent event) {
-        if (event.phase == TickEvent.Phase.END && !taskQueue.isEmpty()) {
-            long currentTick = server.getTickCount();
+        synchronized (taskQueue) {
+            if (event.phase == TickEvent.Phase.END && !taskQueue.isEmpty()) {
+                long currentTick = server.getTickCount();
 
-            // Process tasks whose scheduled execution time has passed
-            while (!taskQueue.isEmpty() && taskQueue.peek().executionTick <= currentTick) {
-                DelayedTask taskToRun = taskQueue.poll();
-                taskToRun.task.run();
+                // Process tasks whose scheduled execution time has passed
+                while (!taskQueue.isEmpty() && taskQueue.peek().executionTick <= currentTick) {
+                    DelayedTask taskToRun = taskQueue.poll();
+                    taskToRun.task.run();
+                }
             }
         }
     }
@@ -60,10 +101,16 @@ public class TaskScheduler {
     private static class DelayedTask implements Comparable<DelayedTask> {
         private final long executionTick;
         private final Runnable task;
+        private final UUID id;
 
-        public DelayedTask(long executionTick, Runnable task) {
+        public DelayedTask(long executionTick, Runnable task, UUID id) {
             this.executionTick = executionTick;
             this.task = task;
+            this.id = id;
+        }
+
+        public UUID getId() {
+            return this.id;
         }
 
         @Override
