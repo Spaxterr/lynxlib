43bbb292ff176f8b76567e1a2d8d3f29736522f3
Kevin Norman <spaxtersurvival@gmail.com>
2024-10-07 04:16:07 +0200

    feat: add method for setting item NBT

--- src/main/java/dev/spaxter/lynxlib/common/ItemUtil.java
+++ src/main/java/dev/spaxter/lynxlib/common/ItemUtil.java
@@ -1,15 +1,22 @@
 package dev.spaxter.lynxlib.common;
 
 import javax.annotation.Nullable;
+
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.JsonToNBT;
 import net.minecraft.nbt.ListNBT;
 import net.minecraft.nbt.StringNBT;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.ResourceLocationException;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.registries.ForgeRegistries;
 
 /**
@@ -68,6 +75,35 @@ public class ItemUtil {
 
         displayTag.put("Lore", loreList);
         nbt.put("display", displayTag);
+    }
+
+    /**
+     * Set the lore for an item stack.
+     *
+     * @param itemStack The item stack to set the lore for
+     * @param lore      The lore to set
+     */
+    @Nullable
+    public static String getItemLore(ItemStack itemStack) {
+        CompoundNBT nbt = itemStack.getTag();
+        if (nbt != null && nbt.contains("display", Constants.NBT.TAG_COMPOUND)) {
+            CompoundNBT displayTag = nbt.getCompound("display");
+            if (displayTag.contains("Lore", Constants.NBT.TAG_LIST)) {
+                ListNBT loreList = displayTag.getList("Lore", Constants.NBT.TAG_STRING);
+                StringBuilder lore = new StringBuilder();
+                for (int i = 0; i < loreList.size(); i++) {
+                    ITextComponent textComponent = ITextComponent.Serializer.fromJson(loreList.getString(i));
+                    if (textComponent != null) {
+                        lore.append(textComponent.getString());
+                        if (i < loreList.size() - 1) {
+                            lore.append("\n");
+                        }
+                    }
+                }
+                return lore.length() > 0 ? lore.toString() : null;
+            }
+        }
+        return null;
     }
 
     /**
